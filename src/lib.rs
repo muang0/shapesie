b03ff4662939eba984e24c24bbcf7887099dd2b5
James <meanderani@gmail.com>
2023-03-12 14:49:26 -0400

    refactor tests, encode_piece fn stub

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,10 @@ fn permute_pieces(pieces: &Vec<Vec<Vec<bool>>>) -> Result<Vec<Vec<Vec<Vec<u8>>>>
     Ok(vec!(vec!(vec!(vec!(0, 1), vec!(1, 0)))))
 }
 
+fn encode_piece(piece: &Vec<Vec<bool>>) -> Vec<Vec<u8>> {
+    vec!(vec!(0b0))
+}
+
 // copies and rotates a piece (90 degrees clockwise)
 fn rotate_piece(piece: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     let mut rotated_piece = vec![vec![false; piece.len()]; piece[0].len()];
@@ -39,52 +43,141 @@ fn attempt_move(board: &mut Vec<Vec<u16>>, pieces: &mut Vec<Vec<Vec<Vec<u8>>>>, 
 mod tests {
     use super::*;
 
+    struct Piecemeal {
+        piece: Vec<Vec<bool>>,
+        piece_flipped: Option<Vec<Vec<bool>>>,
+        piece_rotated: Option<Vec<Vec<bool>>>,
+        piece_encoded: Option<Vec<Vec<u8>>>,
+        piece_permuted: Option<Vec<Vec<Vec<u8>>>>
+    }
+
+    fn calendar_pieces() -> Vec<Piecemeal> {
+        vec!(
+            Piecemeal {
+                piece: vec!(
+                    vec!(1, 1),
+                    vec!(1, 0),
+                    vec!(1, 0),
+                    vec!(1, 0)
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: Some(vec!(
+                    vec!(1, 0),
+                    vec!(1, 0),
+                    vec!(1, 0),
+                    vec!(1, 1),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect()),
+                piece_rotated: Some(vec!(
+                    vec!(1, 1, 1, 1),
+                    vec!(0, 0, 0, 1)
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect()),
+                piece_encoded: Some(vec!(
+                    vec!(0b10110, 0b10001),
+                    vec!(0b11010, 0b01001),
+                    vec!(0b11010, 0b00001),
+                    vec!(0b11000, 0b00001),
+                )),
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(1, 0),
+                    vec!(1, 1),
+                    vec!(1, 0),
+                    vec!(1, 0)
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: None,
+                piece_rotated: None,
+                piece_encoded: None,
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(1, 0, 1),
+                    vec!(1, 1, 1),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: Some(vec!(
+                    vec!(1, 1, 1),
+                    vec!(1, 0, 1)
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect()),
+                piece_rotated: Some(vec!(
+                    vec!(1, 1),
+                    vec!(1, 0),
+                    vec!(1, 1),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect()),
+                piece_encoded: None,
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(0, 0, 1),
+                    vec!(1, 1, 1),
+                    vec!(1, 0, 0),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: None,
+                piece_rotated: None,
+                piece_encoded: None,
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(1, 1, 1),
+                    vec!(1, 1, 1),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: None,
+                piece_rotated: None,
+                piece_encoded: None,
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(1, 0, 0),
+                    vec!(1, 0, 0),
+                    vec!(1, 1, 1),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: None,
+                piece_rotated: None,
+                piece_encoded: None,
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(0, 0, 1, 1),
+                    vec!(1, 1, 1, 0),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: None,
+                piece_rotated: None,
+                piece_encoded: None,
+                piece_permuted: None
+            },
+            Piecemeal {
+                piece: vec!(
+                    vec!(1, 1, 1),
+                    vec!(0, 1, 1),
+                ).iter().map(|y| y.iter().map(|x| *x == 1).collect()).collect(),
+                piece_flipped: None,
+                piece_rotated: None,
+                piece_encoded: None,
+                piece_permuted: None
+            }
+        )
+    }
+    
     #[test]
     fn flips_piece() {
-        let l: Vec<Vec<bool>> = vec!(
-            vec!(1, 0, 0),
-            vec!(1, 0, 0),
-            vec!(1, 1, 1)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        let l_flipped: Vec<Vec<bool>> = vec!(
-            vec!(1, 1, 1),
-            vec!(1, 0, 0),
-            vec!(1, 0, 0)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        let staircase: Vec<Vec<bool>> = vec!(
-            vec!(1, 0, 0, 0),
-            vec!(0, 1, 0, 0),
-            vec!(0, 0, 1, 1)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        let staircase_flipped: Vec<Vec<bool>> = vec!(
-            vec!(0, 0, 1, 1),
-            vec!(0, 1, 0, 0),
-            vec!(1, 0, 0, 0)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        
-            assert_eq!(l_flipped, flip_piece(&l));
-            assert_eq!(staircase_flipped, flip_piece(&staircase));
+        for piecemeal in calendar_pieces() {
+            if let Some(piece_flipped) = piecemeal.piece_flipped {
+                assert_eq!(piece_flipped, flip_piece(&piecemeal.piece));
+            }
         }
+    }
 
     #[test]
     fn rotates_piece() {
-        let field_goal: Vec<Vec<bool>> = vec!(
-            vec!(1, 0, 1),
-            vec!(1, 0, 1),
-            vec!(0, 1, 0),
-            vec!(0, 1, 0)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        let field_goal_rotated: Vec<Vec<bool>> = vec!(
-            vec!(0, 0, 1, 1),
-            vec!(1, 1, 0, 0),
-            vec!(0, 0, 1, 1)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        let z: Vec<Vec<bool>> = vec!(
-            vec!(1, 1, 1, 1),
-            vec!(0, 0, 1, 0),
-            vec!(0, 1, 0, 0),
-            vec!(1, 1, 1, 1)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-        let z_rotated: Vec<Vec<bool>> = vec!(
-            vec!(1, 0, 0, 1),
-            vec!(1, 1, 0, 1),
-            vec!(1, 0, 1, 1),
-            vec!(1, 0, 0, 1)).iter().map(|x| x.iter().map(|y| *y == 1 ).collect()).collect();
-    
-        assert_eq!(field_goal_rotated, rotate_piece(&field_goal));
-        assert_eq!(z_rotated, rotate_piece(&z));
+        for piecemeal in calendar_pieces() {
+            if let Some(piece_rotated) = piecemeal.piece_rotated {
+                assert_eq!(piece_rotated, rotate_piece(&piecemeal.piece));
+            }
+        }
     }
+
 }
