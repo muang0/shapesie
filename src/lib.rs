9e4307554d18cff4ffbafc90a09d1f7067638587
muang0 <meanderani@gmail.com>
2023-03-28 00:33:53 -0400

    test coverage extended to all calendar days

--- src/lib.rs
+++ src/lib.rs
@@ -892,4 +892,56 @@ mod tests {
         let res = solve_puzzle(&board, &pieces).expect("Failed to solve puzzle");
         assert_eq!(expected_board.0, res.0);
     }
+
+    #[test]
+    fn solves_all_possible_days() {
+        // collect calendar pieces
+        let pieces: Vec<Vec<Vec<bool>>> = calendar_pieces()
+            .iter()
+            .map(|piecemeal| piecemeal.piece.clone())
+            .collect();
+
+        // iterate through all possible month, day combinations
+        for month in 1..=12 {
+            for day in 1..=31 {
+                // define board
+                #[rustfmt::skip]
+                let mut board: Vec<Vec<bool>> = vec![
+                    vec![1, 1, 1, 1, 1, 1, 0],
+                    vec![1, 1, 1, 1, 1, 1, 0],
+                    vec![1, 1, 1, 1, 1, 1, 1],
+                    vec![1, 1, 1, 1, 1, 1, 1],
+                    vec![1, 1, 1, 1, 1, 1, 1],
+                    vec![1, 1, 1, 1, 1, 1, 1],
+                    vec![1, 1, 1, 0, 0, 0, 0],
+                ]
+                .iter()
+                .map(|y| y.iter().map(|x| *x == 1).collect())
+                .collect();
+
+                // block off month square on the board
+                let y_index: u8 = (month - 1) / 6;
+                let x_index: u8 = (month - 1) % 6;
+                let month_square: &mut bool = board
+                    .get_mut(usize::from(y_index))
+                    .unwrap()
+                    .get_mut(usize::from(x_index))
+                    .unwrap();
+                *month_square = false;
+
+                // block off day square on the board
+                let y_index: u8 = ((day - 1) / 7) + 2;
+                let x_index: u8 = (day - 1) % 7;
+                let day_square: &mut bool = board
+                    .get_mut(usize::from(y_index))
+                    .unwrap()
+                    .get_mut(usize::from(x_index))
+                    .unwrap();
+                *day_square = false;
+
+                // solve puzzle
+                solve_puzzle(&board, &pieces).expect("Unable to solve puzzle");
+            }
+        }
+    }
 }
